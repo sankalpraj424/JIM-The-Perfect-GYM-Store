15fe73af1605887173bf8384161f19f35c7dd8a4
Sankalp Raj <100083553+SankalpRaj424@users.noreply.github.com>
2023-08-17 10:43:17 +0530

    initial commit

---
+++ src/data/filterBarData.js
@@ -0,0 +1,63 @@
+export const sortMenu = [
+    {
+        id: 1,
+        title: "Latest",
+    },
+    {
+        id: 2,
+        title: "Featured",
+    },
+    {
+        id: 3,
+        title: "Top Rated",
+    },
+    {
+        id: 4,
+        title: "Price(Lowest First)",
+    },
+    {
+        id: 5,
+        title: "Price(Highest First)"
+    },
+];
+
+export const brandsMenu = [
+    {
+        id: 1,
+        label: "ON",
+        checked: false,
+    },
+    {
+        id: 2,
+        label: "Muscleblaaze",
+        checked: false,
+    },
+    {
+        id: 3,
+        label: "Dumbells",
+        checked: false,
+    },
+];
+
+export const categoryMenu = [
+    {
+        id: 1,
+        label: "EQUIPMENTS",
+        checked: false,
+    },
+    {
+        id: 2,
+        label: "SUPPLEMENTS",
+        checked: false,
+    },
+    {
+        id: 3,
+        label: "MACHINES",
+        checked: false,
+    },
+    {
+        id: 4,
+        label: "VITAMINS",
+        checked: false,
+    },
+];
